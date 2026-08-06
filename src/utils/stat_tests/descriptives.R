aeca3a67dda30be550cbe20059f415f63afe61f1
chadcwilliams <cwilliams.uca@gmail.com>
2026-08-06 09:34:51 -0700

    Updated descriptives data table

--- src/utils/stat_tests/descriptives.R
+++ src/utils/stat_tests/descriptives.R
@@ -56,11 +56,30 @@ descriptives = function(input, output, stats, plotdata) {
     }
     #Set outputs
     stats$data_table = descriptives
-    output$data_display = renderRHandsontable(rhandsontable(as.data.frame(data)))
+    
+    # Reshape the raw data into a 2D grid for display
+    ncols = 10  # adjust to however many columns you want per row
+    nrows = ceiling(length(data) / ncols)
+    padded = c(data, rep(NA, nrows * ncols - length(data)))
+    data_grid = as.data.frame(matrix(padded, nrow = nrows, ncol = ncols, byrow = TRUE))
+    colnames(data_grid) = paste0("V", 1:ncols)
+    
+    output$data_display = renderRHandsontable(
+      rhandsontable(
+        data_grid,
+        rowHeaders = FALSE,
+        colHeaders = FALSE,
+        width = "100%",
+        useTypes = FALSE
+      ) %>%
+        hot_table(stretchH = "all") %>%
+        hot_context_menu(FALSE) %>%
+        hot_cols(readOnly = TRUE)
+    )
     output$stats_display = renderRHandsontable({
-        
+      
     })
     output$distribution_display = renderPlot({
-        
+      
     })
 }
